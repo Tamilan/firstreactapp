38612b885536f9099f8feb8d76d8d45201bd5edd
Tamilan <tamilancse089@gmail.com>
2021-03-11 03:25:05 -0500

    test1

--- src/App.js
+++ src/App.js
@@ -29,37 +29,6 @@ function App() {
            
       
 
-        {/* <Nav className="justify-content-end" activeKey="/home">
-          <Nav.Item>
-            <Nav.Link href="/home">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="disabled" disabled>
-              Disabled
-            </Nav.Link>
-          </Nav.Item>
-        </Nav> */}
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
     </AlertProvider>
   );
